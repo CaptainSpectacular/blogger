447919a4a54f364ed7b8d9e149f91a3a89f54a60
pollockcl <pollock.clee@gmail.com>
2018-03-21 17:08:36 -0600

    added tests for article show page

---
+++ spec/features/article_show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'user sees article traits' do
+  describe 'visit articles/:id' do
+    it 'shows article traits' do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+
+      visit articles_path
+      click_link 'Create New Article'
+
+      expect(page).to have_content(article_1.title)
+      expect(page).to have_content(article_1.body)
+    end
+  end
+end
