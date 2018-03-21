86a03dafd433729749a9d93407a73d41d0140b36
pollockcl <pollock.clee@gmail.com>
2018-03-21 17:17:04 -0600

    added path to show page for items with tests

--- spec/features/article_show_spec.rb
+++ spec/features/article_show_spec.rb
@@ -6,7 +6,7 @@ describe 'user sees article traits' do
       article_1 = Article.create!(title: "Title 1", body: "Body 1")
 
       visit articles_path
-      click_link 'Create New Article'
+      click_link article_1.title
 
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_1.body)
