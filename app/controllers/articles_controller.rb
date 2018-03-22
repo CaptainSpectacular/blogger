2a420222bf3e49d901c5c58acb9a97a1dc9b1e3e
pollockcl <pollock.clee@gmail.com>
2018-03-22 15:14:39 -0600

    got rid of empty method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit destroy]
+
   def index
     @articles = Article.all
-  end
-
-  def show
   end
 
   def new
